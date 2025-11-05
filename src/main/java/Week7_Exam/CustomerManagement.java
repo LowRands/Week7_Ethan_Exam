becd031ba265c0e7f0bbbfc9dd4f4aba26aeb8ac
EthanLourens-STUDENT <G00467054@atu.ie>
2025-11-05 16:37:32 +0000

    Customer Management

---
+++ src/main/java/Week7_Exam/CustomerManagement.java
@@ -0,0 +1,16 @@
+package Week7_Exam;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CustomerManagement {
+
+        public static void main(String[] args) {
+            int customerNum = 0;
+            Scanner input = new Scanner(System.in);
+            ArrayList<Customer> Customer = new ArrayList<>();
+
+            System.out.println(": ");
+        }
+    }
+
